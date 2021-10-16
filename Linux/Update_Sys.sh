c128375e7a2ce0658430676adc929f76f8e2a9d7
Steve-Orozco <68754691+Steve-Orozco@users.noreply.github.com>
2021-10-15 22:42:23 -0400

    Add files via upload

---
+++ Linux/Update_Sys.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#updates system in GREEN color!
+
+C=$(printf '\033')
+GREEN="${C}[1;32m"
+B="${C}[1;34m"
+printf "$B" && echo -e "This will update your machine!"
+
+sudo apt update && sudo apt upgrade -yy
+
+prints who is logged into system
+
+printf echo -e "Checking Who is using System"
+
+printf "$GREEN" && w 
+
+
+exit 0
+
